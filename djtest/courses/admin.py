bdc92b6888444822a4c898ee737f6d4d303a8a4d
jerr <mr.jerr@gmail.com>
2016-11-10 19:40:34 +0200

    create courses application

---
+++ djtest/courses/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Courses, Student
+
+# Register your models here.
+
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date')
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birthdate', 'email')
+
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(Student, StudentAdmin)
---
+++ djtest/courses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    birthdate = models.DateField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return '{} <{}>'.format(self.name, self.email)
+
+
+class Courses(models.Model):
+    title = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.title
