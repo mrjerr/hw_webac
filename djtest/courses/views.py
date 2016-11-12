64a6135345cd5756b437c65698eb75ad50404ef0
Eugene Bardosh <mr.jerr@gmail.com>
2016-11-12 05:29:19 +0000

    create forms

--- djtest/courses/views.py
+++ djtest/courses/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django import forms
+from .models import Student
 # Create your views here.
+
+
+# class StudentForm(forms.Form):
+#     name = forms.CharField()
+#     birthdate = forms.DateField()
+#     email = forms.EmailField()
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+def index(request):
+    # form = StudentForm({'name': 'Alex'})
+    form = StudentForm()
+    return render(request, 'index.html', {'form': form})
